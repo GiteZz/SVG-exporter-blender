c7da453dfa649aa82f135391b1494fa970fffca2
gballege <gilles.ballegeer@ugent.be>
2018-06-04 11:56:47 +0200

    changed the way coordinates are calculated, fixed the y axis

---
+++ README.md
@@ -0,0 +1,3 @@
+This is an exporter used to export blender curves to svg
+
+Still creating the exporter, not usable at the moment
--- main.py
+++ main.py
@@ -1,4 +1,40 @@
 import bpy
+
+class svg_handler:
+    def __init__(self, co_min, co_max, margin, min_size):
+        self.margin = margin
+        self.min_size = min_size
+        self.co_min = co_min
+        self.co_max = co_max
+
+        self.x_raw_width = (co_max[0] - co_min[0])
+        self.y_raw_width = (co_max[1] - co_min[1])
+
+        self.scalefactor = max(self.min_size / self.x_raw_width, self.min_size / self.y_raw_width)
+
+        self.x_svg_width_no_margin = self.x_raw_width * self.scalefactor
+        self.x_margin_width = self.x_svg_width_no_margin * (self.margin / 2)
+        self.x_svg_width = self.x_svg_width_no_margin + 2 * self.x_margin_width
+
+        self.y_svg_width_no_margin = self.y_raw_width * self.scalefactor
+        self.y_margin_width = self.y_svg_width_no_margin * (self.margin / 2)
+        self.y_svg_width = self.y_svg_width_no_margin + 2 * self.y_margin_width
+
+    def get_x(self, x_co):
+        return (x_co - self.co_min[0]) * self.scalefactor + self.x_margin_width
+
+    def get_y(self, y_co):
+        y_loc = (y_co - self.co_min[1]) * self.scalefactor
+        return self.y_svg_width - y_loc + self.y_margin_width
+
+    def get_x_str(self, x_co):
+        return str(self.get_x(x_co))
+
+    def get_y_str(self, y_co):
+        return str(self.get_y(y_co))
+
+
+
 ob = bpy.context.object # active object
 
 xmax = -float("inf")
@@ -30,18 +66,7 @@ for p in bezier_points:
 print("minimum is :", xmin)
 print("maximum is :", xmax)
 
-margin = 0.2
-scalefactor = 1
-
-x_raw_width = (xmax - xmin)
-x_svg_width_no_margin = x_raw_width * scalefactor
-x_margin_width = x_svg_width_no_margin * (margin/2)
-x_svg_width = x_svg_width_no_margin + 2 * x_margin_width
-
-y_raw_width = (ymax - ymin)
-y_svg_width_no_margin = y_raw_width * scalefactor
-y_margin_width = y_svg_width_no_margin * (margin/2)
-y_svg_width = y_svg_width_no_margin + 2 * y_margin_width
+handler = svg_handler((xmin, ymin), (xmax, ymax), 0.2, 100)
 
 path_string = ""
 
@@ -52,23 +77,23 @@ amount_point = len(bezier_points)
 
 for i in range(amount_point):
     if i == 0:
-        path_string += "M " + str((bezier_points[0].co[0]-xmin)*scalefactor + x_margin_width)
-        path_string += " " + str((bezier_points[0].co[1]-ymin)*scalefactor + y_margin_width)
+        path_string += "M " + handler.get_x_str(bezier_points[0].co[0])
+        path_string += " " + handler.get_y_str(bezier_points[0].co[1])
 
     path_string += " C "
-    path_string += str((bezier_points[i].handle_right[0] - xmin) * scalefactor + x_margin_width)
+    path_string += handler.get_x_str(bezier_points[i].handle_right[0])
     path_string += " "
-    path_string += str((bezier_points[i].handle_right[1] - ymin) * scalefactor + y_margin_width)
+    path_string += handler.get_y_str(bezier_points[i].handle_right[1])
 
     path_string += ", "
-    path_string += str((bezier_points[(i + 1) % amount_point].handle_left[0] - xmin) * scalefactor + x_margin_width)
+    path_string += handler.get_x_str(bezier_points[(i + 1) % amount_point].handle_left[0])
     path_string += " "
-    path_string += str((bezier_points[(i + 1) % amount_point].handle_left[1] - ymin) * scalefactor + y_margin_width)
+    path_string += handler.get_y_str(bezier_points[(i + 1) % amount_point].handle_left[1])
 
     path_string += ", "
-    path_string += str((bezier_points[(i + 1) % amount_point].co[0] - xmin) * scalefactor + x_margin_width)
+    path_string += handler.get_x_str(bezier_points[(i + 1) % amount_point].co[0])
     path_string += " "
-    path_string += str((bezier_points[(i + 1) % amount_point].co[1] - ymin) * scalefactor + y_margin_width)
+    path_string += handler.get_y_str(bezier_points[(i + 1) % amount_point].co[1])
 
 
 print(path_string)
