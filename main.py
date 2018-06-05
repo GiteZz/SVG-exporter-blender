dd55f5188b8ec17d967d4dc8770b427ec277ba46
gballege <gilles.ballegeer@ugent.be>
2018-06-05 22:46:11 +0200

    added proper method the check if spline is within spline

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import bpy
+import math
 
 class svg_handler:
     def __init__(self, co_min, co_max, margin, min_size):
@@ -75,26 +76,68 @@ class svg_handler:
         path_string += 'z'
         return path_string
 
+def ccw(A,B,C):
+    return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+
+# Return true if line segments AB and CD intersect
+def intersect(A,B,C,D):
+    return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
+def get_point(value, index, P0, P1, P2, P3):
+    return math.pow((1-value),3) * P0[index] + \
+        3 * value * math.pow((1-value), 2) * P1[index] + \
+        3 * math.pow(value,2) * (1 - value) * P2[index] + \
+        math.pow(value,3) * P3[index]
+
+def spline_in_spline(spline2, spline1):
+    line_co_0 = [spline1.bezier_points[0].co[0], spline1.bezier_points[0].co[1]]
+    line_co_1 = [line_co_0[0] + 2*xmax, line_co_0[1] + ymax]
+
+
+    amount_intersection = 0
+    amount_beziers = len(spline2.bezier_points)
+    print(line_co_0)
+    print(line_co_1)
+    for curve_index in range(amount_beziers):
+        P0 = spline2.bezier_points[curve_index].co
+        P1 = spline2.bezier_points[curve_index].handle_right
+        P2 = spline2.bezier_points[(curve_index + 1) % amount_beziers].handle_left
+        P3 = spline2.bezier_points[(curve_index + 1) % amount_beziers].co
+
+        for i in range(10):
+            cob0 = (get_point(i / 10, 0, P0, P1, P2, P3), get_point(i / 10, 1, P0, P1, P2, P3))
+            cob1 = (get_point((i + 1) / 10, 0, P0, P1, P2, P3), get_point((i + 1) / 10, 1, P0, P1, P2, P3))
+            amount_intersection += intersect(line_co_0, line_co_1, cob0, cob1)
+    print(amount_intersection)
+    return amount_intersection % 2 != 0
 
 ob = bpy.context.object # active object
+curves = ob.data.splines
 
 xmax = -float("inf")
+x_max_spline = curves[0]
+
 xmin = float("inf")
+x_min_spline = curves[0]
 
 ymax = -float("inf")
-ymin = float("inf")
-curves = ob.data.splines
-# iterate over points of the curve's first spline
+ymax_spline = curves[0]
 
+ymin = float("inf")
+ymin_spline = curves[0]
+
+# iterate over points of the curve's first spline
 for spline in curves:
     for p in spline.bezier_points:
         if p.co[0] > xmax:
-              xmax = p.co[0]
+            xmax = p.co[0]
+            x_max_spline = spline
         if p.co[0] < xmin:
             xmin = p.co[0]
 
         if p.handle_right[0] > xmax:
-              xmax = p.handle_right[0]
+            xmax = p.handle_right[0]
+            x_max_spline = spline
         if p.handle_right[0] < xmin:
             xmin = p.handle_right[0]
         if p.handle_right[1] > ymax:
@@ -104,6 +147,7 @@ for spline in curves:
 
         if p.handle_left[0] > xmax:
             xmax = p.handle_left[0]
+            x_max_spline = spline
         if p.handle_left[0] < xmin:
             xmin = p.handle_left[0]
         if p.handle_left[1] > ymax:
@@ -111,13 +155,43 @@ for spline in curves:
         if p.handle_left[1] < ymin:
             ymin = p.handle_left[1]
 
+svg_string = '<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n'
+svg_string += '<svg width="120px" height="120px" viewBox="0 0 256 315" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" preserveAspectRatio="xMidYMid">\n'
+
+
 print("minimum is :", xmin)
 print("maximum is :", xmax)
 
 handler = svg_handler((xmin, ymin), (xmax, ymax), 0.2, 100)
 
+path_string = ""
+layers = {}
+amount_curves = len(curves)
+for spline_index1 in range(amount_curves):
+    for spline_index2 in range(spline_index1 + 1,amount_curves,1):
+        if spline_in_spline(curves[spline_index1], curves[spline_index2]):
+            if curves[spline_index1] not in layers:
+                layers[curves[spline_index1]] = [curves[spline_index2]]
+            else:
+                layers[curves[spline_index1]].append(curves[spline_index2])
 
-path_string = handler.curve_to_xml(curves[0].bezier_points, False)
-path_string += handler.curve_to_xml(curves[1].bezier_points, True)
+        if spline_in_spline(curves[spline_index2], curves[spline_index1]):
+            if curves[spline_index2] not in layers:
+                layers[curves[spline_index2]] = [curves[spline_index1]]
+            else:
+                layers[curves[spline_index2]].append(curves[spline_index1])
+
+for key in layers:
+    print(key.bezier_points[0].co)
+    for s in layers[key]:
+        print('/t', s.bezier_points[0].co)
+
+print(layers)
 
 print(path_string)
+svg_string += '<path id="lineAB" d="' + path_string + '" fill="red" />\n'
+svg_string += '</svg>'
+
+file = open("D:/gilles.svg","w")
+file.write(svg_string)
+file.close()
